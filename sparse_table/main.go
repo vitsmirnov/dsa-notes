a5bb0a16eb935929a27f5b8272e6389385611180
Vitaly Smirnov <mrmaybelately@gmail.com>
2026-09-22 10:18:24 +0300

    update sparse_table

--- sparse_table/main.go
+++ sparse_table/main.go
@@ -13,10 +13,13 @@ func MakeMinSparseTable(nums []int) *SparseTable {
 func MakeMaxSparseTable(nums []int) *SparseTable {
 	return MakeSparseTable(nums, func(a, b int) int { return max(a, b) })
 }
+func MakeOrSparseTable(nums []int) *SparseTable {
+	return MakeSparseTable(nums, func(a, b int) int { return a | b })
+}
 
 type SparseTable struct {
-	mins [][]int
-	f    func(int, int) int
+	items [][]int
+	f     func(int, int) int
 }
 
 func MakeSparseTable(nums []int, f func(int, int) int) *SparseTable {
@@ -27,38 +30,37 @@ func MakeSparseTable(nums []int, f func(int, int) int) *SparseTable {
 
 	// levelCount := logs2[numsLen] + 1
 	levelCount := bits.Len(uint(numsLen))
-	mins := make([][]int, levelCount)
-	mins[0] = make([]int, numsLen)
-	copy(mins[0], nums)
+	items := make([][]int, levelCount)
+	items[0] = make([]int, numsLen)
+	copy(items[0], nums)
 	for level := 1; level < levelCount; level++ {
-		mins[level] = make([]int, numsLen-(1<<level)+1)
+		items[level] = make([]int, numsLen-(1<<level)+1)
 		prevLevel := level - 1
 		prevLevelLen := 1 << prevLevel
-		for i := len(mins[level]) - 1; i >= 0; i-- {
-			mins[level][i] = f(mins[prevLevel][i], mins[prevLevel][i+prevLevelLen])
+		for i := range items[level] {
+			items[level][i] = f(items[prevLevel][i], items[prevLevel][i+prevLevelLen])
 		}
 	}
-
 	return &SparseTable{
-		mins: mins,
-		f:    f}
+		items: items,
+		f:     f}
 }
 
-// min/max
-func (st *SparseTable) Query(left, right int) int {
+// idempotent query (min/max, or, etc)
+func (st *SparseTable) IdempQuery(left, right int) int {
 	// l := st.logs2[right-left+1]
 	l := bits.Len(uint(right-left+1)) - 1
-	return st.f(st.mins[l][left], st.mins[l][right-(1<<l)+1])
+	return st.f(st.items[l][left], st.items[l][right-(1<<l)+1])
 }
 
-// sum would be like that
-func (st *SparseTable) Sum(left, right int) int {
+// non-idempotent query (sum, xor etc)
+func (st *SparseTable) NonIdempQuery(left, right int) int {
 	sum := 0
 	length := right - left + 1
-	for i := 0; length != 0; i++ {
+	for level := 0; length != 0; level++ {
 		if length&1 == 1 {
-			sum += st.mins[i][left]
-			left += 1 << i
+			sum = st.f(sum, st.items[level][left])
+			left += 1 << level
 		}
 		length >>= 1
 	}
@@ -87,17 +89,17 @@ func bitLength(n int) int {
 	return count
 }
 
-func testSparseTable() {
+func testSparseTableIdempQuery() {
 	const minNum int = -1e5
 	const maxNum int = 1e5
 	const numsRange = maxNum - minNum + 1
 	const numsLen int = 1e5
 	const testCount int = 100
 	const queryCount int = 100
-	// funcs := [2]func(int, int) int{min, max}
-	funcs := [2]func(int, int) int{
+	funcs := []func(int, int) int{
 		func(a, b int) int { return min(a, b) },
-		func(a, b int) int { return max(a, b) }}
+		func(a, b int) int { return max(a, b) },
+		func(a, b int) int { return a | b }}
 	nums := make([]int, numsLen)
 	t := time.Now()
 	for range testCount {
@@ -110,17 +112,17 @@ func testSparseTable() {
 			length := rand.IntN(numsLen-1) + 1
 			left := rand.IntN(numsLen)
 			right := min(left+length-1, numsLen-1)
-			res1 := st.Query(left, right)
+			res1 := st.IdempQuery(left, right)
 			res2 := agr(nums[left:right+1], f)
 			if res1 != res2 {
 				fmt.Printf("%v != %v: [%v, %v]\n", res1, res2, left, right)
 			}
 		}
 	}
-	fmt.Printf("testSparseTable time: %v\n", time.Since(t))
+	fmt.Printf("testSparseTableIdempQuery time: %v\n", time.Since(t))
 }
 
-func testSparseTableSum() {
+func testSparseTableNonIdempQuery() {
 	const minNum int = -1e2
 	const maxNum int = 1e2
 	const numsRange = maxNum - minNum + 1
@@ -139,14 +141,14 @@ func testSparseTableSum() {
 			length := rand.IntN(numsLen-1) + 1
 			left := rand.IntN(numsLen)
 			right := min(left+length-1, numsLen-1)
-			res1 := st.Sum(left, right)
+			res1 := st.NonIdempQuery(left, right)
 			res2 := agr(nums[left:right+1], sum)
 			if res1 != res2 {
 				fmt.Printf("%v != %v: [%v, %v]\n", res1, res2, left, right)
 			}
 		}
 	}
-	fmt.Printf("testSparseTableSum time: %v\n", time.Since(t))
+	fmt.Printf("testSparseTableNonIdempQuery time: %v\n", time.Since(t))
 }
 
 func agr(nums []int, f func(int, int) int) int {
@@ -174,8 +176,8 @@ func main() {
 	// fmt.Println(st.Query(0, numsLen-1))
 	// return
 
-	testSparseTable()
-	testSparseTableSum()
+	testSparseTableIdempQuery()
+	testSparseTableNonIdempQuery()
 }
 
 //            l             r
